249f1a5b2fd03874ee2191abe99999d03fbd0f8a
Aryanando PC Kantor <aryanando232@gmail.com>
2025-02-10 12:26:35 +0700

    Adding Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    print("Pausing Progress");
+    await Future.delayed(const Duration(seconds: 3));
+    print("Unpausing Progress");
+    FlutterNativeSplash.remove();
+  }
 
   @override
   Widget build(BuildContext context) {
